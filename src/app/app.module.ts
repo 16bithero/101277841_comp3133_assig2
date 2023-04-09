5d946fcac2e5390afdd23d372181dcc9ecbddc82
16bithero <renzzi.adorador@gmail.com>
2023-04-08 21:44:45 -0400

    Added needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    SignupComponent,
+    AddEmployeeComponent,
+    ViewEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
